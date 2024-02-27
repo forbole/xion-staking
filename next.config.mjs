badcb58aa47e116e34fef9a916d4d9df5f55ca0c
Ignacio <ignacio@forbole.com>
2024-02-27 15:58:08 +0800

    feat: add back gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,24 @@
+const useProxy = false;
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.QUICK_BUILD === "true",
   },
-  rewrites: async () => [
-    {
-      destination: "https://rpc.xion-testnet-1.burnt.com:443",
-      source: "/rpc",
-    },
-  ],
   typescript: {
     ignoreBuildErrors: process.env.QUICK_BUILD === "true",
   },
+  ...(useProxy
+    ? {
+        rewrites: async () => [
+          {
+            destination: "https://rpc.xion-testnet-1.burnt.com:443",
+            source: "/rpc",
+          },
+        ],
+      }
+    : {
+        output: "export",
+      }),
 };
 
 export default nextConfig;
--- src/features/staking/lib/constants.ts
+++ src/features/staking/lib/constants.ts
@@ -5,7 +5,9 @@ export const { chainId } = testnetChainInfo;
 export const dashboardUrl =
   process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
 
-export const rpcEndpoint =
-  typeof window === "undefined"
-    ? "https://rpc.xion-testnet-1.burnt.com:443"
-    : `${window.location.origin}/rpc`;
+// export const rpcEndpoint =
+//   typeof window === "undefined"
+//     ? "https://rpc.xion-testnet-1.burnt.com:443"
+//     : `${window.location.origin}/rpc`;
+
+export const rpcEndpoint = "https://rpc.xion-testnet-1.burnt.com:443";
