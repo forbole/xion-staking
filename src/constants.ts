4049b6ef048ceeaf21d20cfe8a328c12e9c909cc
justin <328965+justinbarry@users.noreply.github.com>
2024-03-22 11:38:44 -0700

    Display XION balance in full precision

--- src/constants.ts
+++ src/constants.ts
@@ -28,4 +28,4 @@ export const unbondingDays = isTestnet ? 3 : 21;
 // Arbitrary value to avoid using a bigger fee than the actual reward
 export const minClaimableXion = 0.00001;
 
-export const minDisplayedXion = 0.001;
+export const minDisplayedXion = 0.000001;
