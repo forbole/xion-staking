29636cd5d28df5496f8387bfbbc3a03c73de07fe
Burnt Nerve <seanlbray@gmail.com>
2024-07-17 16:11:36 -0500

    Fixed unexported constant

--- src/config.ts
+++ src/config.ts
@@ -26,15 +26,16 @@ export const BASE_PATH = getEnvBooleanOrThrow(
 // The contract address for the faucet
 export const FAUCET_CONTRACT_ADDRESS =
   "xion132dxh4k3dpyalq6tfq7006h8kpk3m30f4mwc5dgqefy6akudm50s96mn6q";
-export const NETWORK_TYPE = getEnvStringOrThrow(
-  "NEXT_PUBLIC_NETWORK_TYPE",
-  process.env.NEXT_PUBLIC_NETWORK_TYPE,
-);
 
 // The RPC endpoint for the network
 export const RPC_ENDPOINT = getEnvStringOrThrow(
   "NEXT_PUBLIC_RPC_ENDPOINT",
   process.env.NEXT_PUBLIC_RPC_ENDPOINT,
+);
+
+const NETWORK_TYPE = getEnvStringOrThrow(
+  "NEXT_PUBLIC_NETWORK_TYPE",
+  process.env.NEXT_PUBLIC_NETWORK_TYPE,
 );
 
 export const IS_TESTNET = NETWORK_TYPE === "testnet";
