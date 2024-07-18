4e834bff689d07d5e174a25a3ef4b4f2c770eb4e
Burnt Nerve <seanlbray@gmail.com>
2024-07-17 20:11:20 -0500

    Added url config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,18 @@ import { Analytics } from "@vercel/analytics/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { FAUCET_CONTRACT_ADDRESS, RPC_ENDPOINT } from "@/config";
+import { FAUCET_CONTRACT_ADDRESS } from "@/config";
 import BaseWrapper from "@/features/core/components/base-wrapper";
 import { CoreProvider } from "@/features/core/context/provider";
 import { StakingProvider } from "@/features/staking/context/provider";
 
 import "./globals.css";
+import { REST_URL, RPC_URL } from "@/constants";
 
 const abstraxionConfig = {
   contracts: [FAUCET_CONTRACT_ADDRESS],
-  rpcUrl: RPC_ENDPOINT,
+  restUrl: REST_URL,
+  rpcUrl: RPC_URL,
   stake: true,
 };
 
